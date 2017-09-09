06d51f33ede803c9d8d95786018706a594d8c6db
gideonflynn <allmynameswastaken@gmail.com>
2017-09-09 13:24:31 +0200

    Remove old way to query

--- src/data/schema.js
+++ src/data/schema.js
@@ -1,28 +1,53 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
+import { merge } from 'lodash';
+import { makeExecutableSchema, addErrorLoggingToSchema } from 'graphql-tools'; // eslint-disable-line no-unused-vars
 
 import {
-  GraphQLSchema as Schema,
-  GraphQLObjectType as ObjectType,
-} from 'graphql';
+  schema as DatabaseSchema,
+  resolvers as DatabaseResolvers,
+  mutations as DatabaseMutations,
+  queries as DatabaseQueries,
+} from './types/Database/schema';
 
-import me from './queries/me';
-import news from './queries/news';
+const logger = { log: e => console.error(e.stack) };
 
-const schema = new Schema({
-  query: new ObjectType({
-    name: 'Query',
-    fields: {
-      me,
-      news,
-    },
-  }),
+const RootQuery = [
+  `
+  type RootQuery {
+    ${DatabaseQueries}
+  }
+`,
+];
+
+const Mutation = [
+  `
+  type Mutation {
+    ${DatabaseMutations}
+  }
+`,
+];
+
+const SchemaDefinition = [
+  `
+  schema {
+    query: RootQuery
+    mutation: Mutation
+  }
+`,
+];
+
+// Merge all of the resolver objects together
+// Put schema together into one array of schema strings
+const resolvers = merge(DatabaseResolvers);
+
+const schema = [
+  ...SchemaDefinition,
+  ...RootQuery,
+  ...DatabaseSchema,
+  ...Mutation,
+];
+
+export default makeExecutableSchema({
+  typeDefs: schema,
+  resolvers,
+  logger,
 });
-
-export default schema;
